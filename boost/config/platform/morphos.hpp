cae5a0fecdf50f8357b48d432964a8b689ab6d87
Andre Geisler <andre@exdev.de>
2025-01-19 17:50:56 +0100

    add morphos.hpp

---
+++ boost/config/platform/morphos.hpp
@@ -0,0 +1,24 @@
+//  (C) Copyright Andre Geisler 2025.
+//  Use, modification and distribution are subject to the 
+//  Boost Software License, Version 1.0. (See accompanying file 
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org for most recent version.
+
+#define BOOST_PLATFORM "MorphOs"
+
+#ifdef __cplusplus
+    #include <cstdlib>
+#else
+    #include <stdlib.h>
+#endif
+
+// required by boost-chrono
+#ifndef __STDC_LIMIT_MACROS
+    #define __STDC_LIMIT_MACROS
+#endif
+
+//#  ifndef BOOST_POSIX_API
+//#    define BOOST_POSIX_API
+//#  endif
+//#include <boost/config/detail/posix_features.hpp>
